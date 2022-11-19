25d9bd00a05850cfd61e00389f605d7c0c00de36
Marco Weingart <weingart.marco@hotmail.de>
2022-11-19 16:42:56 +0100

    store and query database settings

---
+++ webapp/src/get_db_login.php
@@ -0,0 +1,18 @@
+<?php
+    function get_db_login($db_user) {
+        // Read and decode settings.json
+        $settings_file = file_get_contents(__DIR__.'/../../database/settings.json');
+        $settings = json_decode($settings_file,true);
+
+        // Extract information
+        $fqdn = $settings["general"]["fqdn"];
+        $user = $settings["users"][$db_user]["username"];
+        $password = $settings["users"][$db_user]["password"];
+        $database = $settings["general"]["database"];
+        $port = $settings["general"]["port"];
+        $socket = $settings["general"]["socket"];
+
+        // Return as array
+        return array($fqdn, $user, $password, $database, $port, $socket);
+    }
+?>
