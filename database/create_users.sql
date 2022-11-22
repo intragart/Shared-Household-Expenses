fb6ad339d33076c6015fca9d8d388cb94eebcc5a
Marco Weingart <weingart.marco@hotmail.de>
2022-11-22 21:06:19 +0100

    new user she_editor

--- database/create_users.sql
+++ database/create_users.sql
@@ -11,3 +11,11 @@ GRANT SELECT ON shared_household_expenses.article_list TO 'she_viewer'@'localhos
 DROP USER IF EXISTS 'she_login'@'localhost';
 CREATE USER 'she_login'@'localhost' IDENTIFIED BY '1234';
 GRANT EXECUTE ON FUNCTION shared_household_expenses.login TO 'she_login'@'localhost';
+
+DROP USER IF EXISTS 'she_editor'@'localhost';
+CREATE USER 'she_editor'@'localhost' IDENTIFIED BY '1234';
+GRANT INSERT ON shared_household_expenses.contribution TO 'she_editor'@'localhost';
+GRANT INSERT ON shared_household_expenses.purchase TO 'she_editor'@'localhost';
+GRANT INSERT ON shared_household_expenses.retailer TO 'she_editor'@'localhost';
+GRANT SELECT ON shared_household_expenses.retailer TO 'she_editor'@'localhost';
+GRANT SELECT ON shared_household_expenses.dashboard TO 'she_editor'@'localhost';
