c3a355b4a96827fdb11151c9233ab34b03a34824
Marco Weingart <weingart.marco@hotmail.de>
2022-12-29 22:15:00 +0100

    show active users

--- webapp/www/settings.php
+++ webapp/www/settings.php
@@ -15,6 +15,13 @@
     // You should have received a copy of the GNU General Public License
     // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+    // Get the informations to connect to database
+    require("../src/get_db_login.php");
+    $db_settings = get_db_login("admin");
+
+    // connect to database
+    $db = new MySQLi($db_settings[0], $db_settings[1], $db_settings[2], $db_settings[3], $db_settings[4], $db_settings[5]);
+
     // Get the settings for the used currency
     require_once("../src/get_currency_settings.php");
     $currency = get_currency_settings();
@@ -29,6 +36,7 @@
         <link type="text/css" rel="stylesheet" href="/style/css/navigation.css">
         <link type="text/css" rel="stylesheet" href="/style/css/form-structure.css">
         <link type="text/css" rel="stylesheet" href="/style/css/form-detailed.css">
+        <link type="text/css" rel="stylesheet" href="/style/css/table.css">
         <meta charset="UTF-8">
         <meta name="keywords" content="">
         <meta name="description" content="">
@@ -84,8 +92,60 @@
                         </div>
                     </div>
                 </form>
-                
-                <h2>Nutzereinstellungen</h2>
+                <form autocomplete="off" id="updateUsers" class="detailed-form" novalidate>
+                    <div class="form-row">
+                        <h2>Nutzereinstellungen</h2>
+                    </div>
+                    <div class="form-row">
+                        <?php
+                            try {
+                                // Get table data
+                                $select_statement = "SELECT user_id, username, pretty_name, start_value, account_status FROM user WHERE account_status <> 'DEACTIVATED'";
+                                $res = $db->query($select_statement);
+                        
+                                // Create Table Head if data has been received
+                                if ($res->num_rows > 0) {
+                                    echo "<table class=\"simpletable\">\n";
+                                    echo "<tr class=\"simpletable\">\n";
+                                    echo "<th class=\"simpletable\">Nutzername</th>\n";
+                                    echo "<th class=\"simpletable\">Anzeigename</th>\n";
+                                    echo "<th class=\"simpletable\">Startwert</th>\n";
+                                    echo "<th class=\"simpletable\">Status</th>\n";
+                                    echo "</tr>\n";
+                                } else {
+                                    echo "<p>No Data.</p>";
+                                }
+                        
+                                // Display the received data in table
+                                while ($row = $res->fetch_assoc()) {
+                                    echo "<tr class='simpletable' id='user-".$row['user_id']."'>";
+                                    echo "<td class=\"simpletable\">".$row['username']."</td>";
+                                    echo "<td class=\"simpletable\">".$row['pretty_name']."</td>";
+                                    echo "<td class=\"simpletable\">";
+                                    if ($currency["currencyPosition"] == "before") { echo $currency["currencySymbol"]." "; }
+                                    echo str_replace(".", $currency["currencyDecimal"], $row['start_value']);
+                                    if ($currency["currencyPosition"] == "after") { echo " ".$currency["currencySymbol"]; }
+                                    echo "</td>";
+                                    echo "<td class=\"simpletable\">".$row['account_status']."</td>";
+                                    echo "</tr>";
+                                }
+                        
+                                // Close Table if data has been received
+                                if ($res->num_rows > 0) {
+                                    echo "</table>";
+                                }
+                        
+                            } catch (Exception $ex) {
+                                echo "Error during SQL Execution.".$ex;
+                            }
+                        ?>
+                    </div>
+                    <div class="form-row">
+                        <div class="input-100 input-r">
+                            <input id="BtnNewUser" class="submit btn" type="button" value="Neuer Nutzer" onclick="">
+                        </div>
+                    </div>
+                </form>
                 <br />
             </div>
         </div>
