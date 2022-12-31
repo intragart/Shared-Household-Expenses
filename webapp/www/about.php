c231190befd847858dd5bc62862549d0137d2f04
Marco Weingart <weingart.marco@hotmail.de>
2022-12-31 14:08:42 +0100

    added Changelog

--- webapp/www/about.php
+++ webapp/www/about.php
@@ -66,6 +66,9 @@
                 <p>
                     <div class="github-card" data-github="lepture/github-cards" data-width="400" data-height="177" data-theme="default"></div>
                 </p>
+                <p>
+                <div class="github-card" data-github="olivierlacan/keep-a-changelog" data-width="400" data-height="177" data-theme="default"></div>
+                </p>
                 <h2>Autor</h2>
                 <p>
                     <div class="github-card" data-github="intragart" data-width="400" data-height="" data-theme="default"></div>
